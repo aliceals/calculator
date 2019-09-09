0680fbbda31f7265fa077e767deda34ba56304a5
Alice Alsford <alice.alsford@gmail.com>
2019-09-09 21:27:21 +1200

    added event listeners for js

--- main.js
+++ main.js
@@ -1,80 +1,42 @@
-//create empty array to store entries
-var entries = [];
 
-//create total variable and set to 0
-var total = 0;
+var decimal = document.getElementById("decimal");
+var clear = document.getElementById("clear");
+//select element that holds input and result
+var display = document.getElementById("display");
+//select the buttons 0-9
+var btnNumbers = document.getElementsByClassName("btn--number");
+//select operators
+var btnOperators = document.getElementsByClassName("operator");
 
-//create temp variable and set to empty string
-var temp = '';
 
-//on click of button run function and turn variable val into the text of the button clicked
-$("button").on('click', function () {
-    var val = $(this).text();
+//create three variables. One to hold create value, one to hold pending value and one to hold array to be eval
+var currentValue = 0;
+var pastValues;
+var arrayEval = [];
 
-    // Got a number, add to temp. Check if it is a number then add val to the temp variable
-    if (!isNaN(val) || val === '.') {
-        temp += val;
-        $("#answer").val(temp.substring(0, 10));
+//add event listeners to buttons 
 
-        // Got some symbol other than equals, add temp to our entries
-        // then add our current symbol and clear temp
-        //check if value is AC then clear temp to nothing
-    } else if (val === 'AC') {
-        entries = [];
-        temp = '';
-        total = 0;
-        $("#answer").val('')
+for (i = 0; i < btnNumbers.length; i++) {
+    btnNumbers[i].addEventListener("click", updateDisplayVal)
+}
 
-        // Check if val is CE. If so clear clear last entry
-    } else if (val === 'CE') {
-        temp = '';
-        $("#answer").val('')
+for (i = 0; i < btnOperators.length; i++) {
+    btnOperators[i].addEventListener("click", updateDisplayVal)
+}
 
-        // if value is equal to x then add multiply symbol to work with eval
-    } else if (val === 'x') {
-        entries.push(temp);
-        entries.push('*');
-        temp = '';
 
-        // if value is equal to divide. Then Change divide symbol to work with eval
-    } else if (val === '÷') {
-        entries.push(temp);
-        entries.push('/');
-        temp = '';
+function updateDisplayVal(e) {
 
-        // If value is equal to the equals sign then perform calculation
-    } else if (val === '=') {
-        entries.push(temp);
-
-        //run through array of entries and do the calculation as numbers and symbols come up. 
-        //If value is a symbol then use symbol and go onto next number
-
-        var nt = Number(entries[0]);
-        for (var i = 1; i < entries.length; i++) {
-            var nextNum = Number(entries[i + 1])
-            var symbol = entries[i];
-
-            if (symbol === '+') { nt += nextNum; }
-            else if (symbol === '-') { nt -= nextNum; }
-            else if (symbol === '*') { nt *= nextNum; }
-            else if (symbol === '/') { nt /= nextNum; }
-
-            i++;
-        }
-
-        // Swap the '-' symbol so text input handles it correctly
-        if (nt < 0) {
-            nt = Math.abs(nt) + '-';
-        }
-
-        $("#answer").val(nt);
-        entries = [];
-        temp = '';
-
-        // Push number
-    } else {
-        entries.push(temp);
-        entries.push(val);
-        temp = '';
+    var btnText = e.target.value;
+    console.log(btnText);
+    if (currentValue === '0') {
+        currentValue = '';
     }
-});
+    currentValue += btnText;
+    display.innerText = currentValue;
+}
+
+
+
+
+
