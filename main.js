bc796171043f9fa1624daeadcd9cd1424e704cc0
Alice Alsford <alice.alsford@gmail.com>
2019-09-11 20:47:41 +1200

    calculation working

--- main.js
+++ main.js
@@ -17,14 +17,17 @@ var arrayEval = [];
 //add event listeners to buttons 
 
 for (i = 0; i < btnNumbers.length; i++) {
-    btnNumbers[i].addEventListener("click", updateDisplayVal)
+    btnNumbers[i].addEventListener("click", updateDisplayVal);
+    btnNumbers[i].addEventListener("click", performOperation)
 }
 
 for (i = 0; i < btnOperators.length; i++) {
-    btnOperators[i].addEventListener("click", updateDisplayVal)
+    btnOperators[i].addEventListener("click", updateDisplayVal);
+    btnOperators[i].addEventListener("click", performOperation)
+
 }
 
-
+//get value from buttons
 function updateDisplayVal(e) {
 
     var btnText = e.target.value;
@@ -34,6 +37,76 @@ function updateDisplayVal(e) {
     }
     currentValue += btnText;
     display.innerText = currentValue;
+}
+
+
+
+//function to do calculation
+//add all buttons to the array
+
+function performOperation(e) {
+    var operator = e.target.value;
+    debugger;
+    if (operator === '+') {
+        arrayEval.push('+');
+        display.value = "+"
+    } else if (operator === '-') {
+        arrayEval.push('-');
+        display.value = "-"
+    } else if (operator === '/') {
+        arrayEval.push('/')
+        display.value = "/"
+    } else if (operator === 'x') {
+        arrayEval.push('*')
+        display.value = "x"
+    } else if (operator === '.') {
+        arrayEval.push('.')
+        display.value = "."
+    } else if (operator === '1') {
+        arrayEval.push(1)
+        display.value = "1"
+    } else if (operator === '2') {
+        arrayEval.push(2)
+        display.value = "2"
+    } else if (operator === '3') {
+        arrayEval.push(3)
+        display.value = "3"
+    } else if (operator === '4') {
+        arrayEval.push(4)
+        display.value = "4"
+    } else if (operator === '5') {
+        arrayEval.push(5)
+        display.value = "5"
+    } else if (operator === '6') {
+        arrayEval.push(6)
+        display.value = "6"
+    } else if (operator === '7') {
+        arrayEval.push(7)
+        display.value = "7"
+    } else if (operator === '8') {
+        arrayEval.push(8)
+        display.value = "8"
+    } else if (operator === '9') {
+        arrayEval.push(9)
+        display.value = "9"
+    } else if (operator === '0') {
+        arrayEval.push(0)
+        display.value = "0"
+    } else if (operator === "=") {
+        var evaluation = eval(arrayEval.join(""));
+        display.value = evaluation;
+        arrayEval = [];
+    }
+    console.log(arrayEval);
+
+}
+
+
+clear.onclick = () => {
+    currentValue = "0";
+    pastValues = undefined;
+    arrayEval = [];
+    display.value = 0;
 }
 
 
